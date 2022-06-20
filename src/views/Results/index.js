2340475438a07a2ffa803d6f2722c8967e1f6d40
Jesús Gonzales <gmanriqe@gmail.com>
2022-06-20 14:13:08 -0500

    LOADING - creacion de loading en la lista de pelicula como resultado

--- src/views/Results/index.js
+++ src/views/Results/index.js
@@ -2,19 +2,18 @@ import moviesImage from '../../assets/images/into_search.png'
 
 import { useParams } from 'react-router-dom';
 import {useFetchMoviesQuery} from '../../redux/api/movies';
-// import { useFetchMoviesQuery } from './../../redux/api/movies';
-
+import Loading from './components/loading';
 
 const Results = () => {
     const { title } = useParams();
-    // const { data: movies, loading, isSuccess, isFetching, error } = useFetchMoviesQuery(title);
-    const { data  } = useFetchMoviesQuery(title);
+    const { data: movies, isLoading, isSuccess, isFetching, error } = useFetchMoviesQuery(title);
 
-    console.log(data)
+    console.log(movies)
     return (
         <div className="flex flex-row">
-            <div className="w-3/5">
-
+            <div className="w-3/5 px-18">
+                {/* si esta cargando  y también esta haciendo fetching, entonces que me muestre el <Loading/> */}
+                {isLoading && isFetching && <Loading />}
             </div>
             <div className="w-2/5">
                 <img src={moviesImage} className="h-screen w-full object-cover" alt="chair" />
