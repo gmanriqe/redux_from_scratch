679871f525ba15361bec624ef9fe278251d0545b
Jesús Gonzales <gmanriqe@gmail.com>
2022-06-22 21:46:06 -0500

    BANDERAS RTK - evaluando las banderas que nos brinda rtk query

--- src/views/Results/index.js
+++ src/views/Results/index.js
@@ -1,20 +1,21 @@
 import moviesImage from '../../assets/images/into_search.png'
 
 import { useParams } from 'react-router-dom';
-import {useFetchMoviesQuery} from '../../redux/api/movies';
+import { useFetchMoviesQuery } from '../../redux/api/movies';
 import Loading from './components/loading';
 import List from './components/List';
 
 const Results = () => {
     const { title } = useParams();
-    const { data: movies, isLoading, isSuccess, isFetching, error } = useFetchMoviesQuery(title);
+    const { data: movies, isLoading, isSuccess, isFetching, error } = useFetchMoviesQuery(title); //  isLoading = si esta cargando la informacion | isFetching = si esta trayendo la información.
 
     console.log(movies)
     return (
         <div className="flex flex-row">
             <div className="w-3/5 px-18">
-                {/* si esta cargando  y también esta haciendo fetching, entonces que me muestre el <Loading/> */}
-                {isLoading && isFetching ? <Loading /> : <List data={movies?.results}/>}
+                {error && <p>Hubo un error con la busqueda de tú película.</p>}{/* si ocurre un error, muestro un texto */}
+                {isLoading || isFetching ? <Loading /> : undefined}  {/* si esta cargando o si está trayendo información (esta haciendo fetching), muestrame el loading; sino no muestres nada. */}
+                {isSuccess && movies?.results ? <List data={movies?.results} /> : undefined} {/* si es success y movies.results existe, mostraremos resultados; sino no muestro nada */}
             </div>
             <div className="w-2/5">
                 <img src={moviesImage} className="h-screen w-full object-cover" alt="chair" />
