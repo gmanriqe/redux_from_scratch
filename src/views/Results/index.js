2e305235149659376db0223b3549d5b7a9d47d24
Jesús Gonzales <gmanriqe@gmail.com>
2022-06-22 21:59:22 -0500

    BANDERAS RTK - evaluando las banderas que nos brinda rtk query, pero esta vez con un codigo mas legible (en una funcion independiente)

--- src/views/Results/index.js
+++ src/views/Results/index.js
@@ -9,13 +9,28 @@ const Results = () => {
     const { title } = useParams();
     const { data: movies, isLoading, isSuccess, isFetching, error } = useFetchMoviesQuery(title); //  isLoading = si esta cargando la informacion | isFetching = si esta trayendo la información.
 
-    console.log(movies)
+    const renderContent = () => {
+        // si ocurre un error, muestro un texto.
+        if (error) {
+            return (<div className="flex items-center justify-center h-full">
+                <p className="text-xl">Hubo un error con la busqueda de tú película.</p>
+            </div>)
+        }
+
+        // si esta cargando o si está trayendo información (esta haciendo fetching), muestrame el loading; sino no muestres nada.
+        if (isLoading || isFetching) {
+            return <Loading />
+        }
+
+        // si es success y movies.results existe, mostraremos resultados; sino no muestro nada.
+        if (isSuccess && movies?.results) {
+            return <List data={movies?.results} />
+        }
+    }
     return (
         <div className="flex flex-row">
             <div className="w-3/5 px-18">
-                {error && <p>Hubo un error con la busqueda de tú película.</p>}{/* si ocurre un error, muestro un texto */}
-                {isLoading || isFetching ? <Loading /> : undefined}  {/* si esta cargando o si está trayendo información (esta haciendo fetching), muestrame el loading; sino no muestres nada. */}
-                {isSuccess && movies?.results ? <List data={movies?.results} /> : undefined} {/* si es success y movies.results existe, mostraremos resultados; sino no muestro nada */}
+                {renderContent()}
             </div>
             <div className="w-2/5">
                 <img src={moviesImage} className="h-screen w-full object-cover" alt="chair" />
